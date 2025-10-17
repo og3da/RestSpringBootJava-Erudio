7651ace0c43b7fa5badd86e8900b114726e7497d
og3da <rapha488@gmail.com>
2025-10-16 23:59:14 -0300

    first steps in spring boot 3.4.0

---
+++ src/main/java/br/com/og3da/demo/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.og3da.demo.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/br/com/og3da/demo/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.og3da.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
